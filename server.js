ae45549a5b069c550de5eac0ec8fb2d49de24f13
Madison (Pfaff) Edgar <withmadico@gmail.com>
2019-10-23 10:43:42 -0400

    :art: Fix formatting of 3 files for commit 3662758 (#82)
    woop woop

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
 const db = require(`./config/db`);
-const translations = require('./app/routes');
+const translations = require("./app/routes");
 
 const port = 8000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parser is being depricated, have to use the new parser
-MongoClient.connect(db.url, { useNewUrlParser: true}, (err, database) => {
-  if (err) {
-    return console.log(err);
-  }
-  // Make sure to add the database name and not the collection name
-  const translationDatabase = database.db("legesher-translations");
-  translations(app, translationDatabase);
+MongoClient.connect(db.url, {
+    useNewUrlParser: true
+}, (err, database) => {
+    if (err) {
+        return console.log(err);
+    }
+    // Make sure to add the database name and not the collection name
+    const translationDatabase = database.db("legesher-translations");
+    translations(app, translationDatabase);
 });
 
 app.listen(port, () => {
-  console.log("Holla we're live on port "+port+" babaayyy!!!");
+    console.log("Holla we're live on port " + port + " babaayyy!!!");
 });
